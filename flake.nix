5942ee5994b9651c02e3d87b73280c5bcbed53f4
max-amb <max_a@e.email>
2025-11-19 10:46:25 +0000

    Updated flake, moved around external modules

--- flake.nix
+++ flake.nix
@@ -3,23 +3,37 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-          };
-        in
-        with pkgs;
-        {
-          devShells.default = mkShell {
-            buildInputs = [ clang cmake eigen_3_4_0 pkg-config  python313Packages.pybind11 python313 ];
-                    shellHook = ''
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            clang
+            cmake
+            eigen_3_4_0
+            pkg-config
+            python313Packages.pybind11
+            python313Packages.numpy
+            python313Packages.gymnasium
+            python313
+          ];
+          shellHook = ''
             export PYTHON_EXECUTABLE="${python313}/bin/python3"
             export Python3_ROOT_DIR="${python313}"
           '';
-          };
-        }
-      );
+        };
+      }
+    );
 }
